7cf33986f21dae96e3f9cedd59008365cc389bf3
kcsic <kcsic2000@gmail.com>
2026-06-03 05:08:30 +0900

    cstify

---
+++ src/main/java/com/cstify/CstifyApplication.java
@@ -0,0 +1,11 @@
+package com.cstify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CstifyApplication {
+    static void main(String[] args) {
+        SpringApplication.run(CstifyApplication.class, args);
+    }
+}
---
+++ src/test/java/com/cstify/CstifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cstify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CstifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
