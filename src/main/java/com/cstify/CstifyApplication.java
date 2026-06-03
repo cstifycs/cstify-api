fe4e65e1b4dfe361e146cdb845f6a40b2e605aeb
kcsic <kcsic2000@gmail.com>
2026-06-03 23:06:12 +0900

    modulith 적용

--- src/main/java/com/cstify/CstifyApplication.java
+++ src/main/java/com/cstify/CstifyApplication.java
@@ -2,7 +2,9 @@ package com.cstify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class CstifyApplication {
     static void main(String[] args) {
